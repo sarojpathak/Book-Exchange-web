f5369353ccd65aaf841f8a79870decde8498fa1a
YagyaBhatta83 <bhattayagya123@gmail.com>
2020-06-25 12:51:42 +0545

    user registraton test case writing

--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -4,25 +4,27 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\User;
 
 class UserRegistrationTest extends TestCase
 {
     /** @test */
     public function a_user_can_register_into_system()
     {
-        $this->withoutExceptionHandling();
-        $response = $this->post('/users');
-        $response->assertStatus(200);
-
+        
+        // $response = $this->post('/users');
         // $response->assertStatus(200);
-        // $response = $this->post('/users',[
-        //     'full_name'=>'Cool Book Title',
-        //     'address'=>'Victor',
-        //     'email'=>'Victor@gmail.com',
-        //     'phone'=>'1234567898'
-        // ]);
 
-        // $response->assertOK();
-        // $response->assertCount(1,User::all());
+       $this->withoutExceptionHandling();
+        $response = $this->post('/users/register',[
+            'name' => 'Victor',
+            'email' => 'Victor@gmail.com',
+            'password' => 'V123456',
+            'phone' => '1234567890',
+            'address'=> 'ktm'
+        ]);
+
+        $response->assertOK();
+        $response->assertCount(1,User::all());
     }
 }
