efd4528179247beef7456428ca9da05683977d7c
bethebikash <bhattaraibikash55@gmail.com>
2020-07-06 12:05:11 +0545

    Home Page- Book List and Depails of book design

--- resources/views/front/homepage.blade.php
+++ resources/views/front/homepage.blade.php
@@ -18,31 +18,27 @@
    </div>
 </header>
 
+
+<!-- Book list section -->
 <div class="container">
-  <div class="row">
-    @foreach ($books as $book)
-  <div class="col-md-4">
-    <div class="card">
-      <div class="card-header text-center">
-        {{$book->name}}
-      </div> 
-      <div>
-          <div class="container mt-2" style="height: 200px; width:200px;">
-            <img src="{{$book->image}}" class="img-fluid" alt="Responsive image">
-        </div> 
-      <div  class="card-body">  <p class="card-text">{{Str::limit($book->description,100)}}</p>
-        <a href="{{route('book-detail',$book->id)}}" class="btn btn-primary">Read More...</a></div>
+@foreach ($books as $book)
+    <div class="card mb-4">
+      <div class="card-body">
+      <div class="row">
+        <div class="col-md-6">
+          <img class="img-thumbnail" src={{asset('storage/'.($book->image))}} alt="Book Image"/>
+        </div>
+        <div class="col-md-6">
+          <h3>{{$book->name}}</h3>
+          <h6 class="text-muted">{{$book->author}}</h6>
+          <p>{{Str::limit($book->description,50)}}
+          </p>
+          <a href="{{route('book-detail',$book->id)}}" class="btn btn-primary border-light">Exchange</a>
+        </div>
       </div>
     </div>
-    <div class="card" style="width: 200; height:200">
-        <img src="{{asset('storage/'.($book->image))}}" class="img" alt="Responsive image">
-  <div class="card-body">
-  <h5 class="card-title">{{$book->name}}</h5>
-    <p class="card-text">{{$book->description}}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
   @endforeach  
 </div>
 
-</div>
 @endsection
